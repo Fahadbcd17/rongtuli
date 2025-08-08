72794c76a0fdbc1756c54e4c1ac3841b0dc20db3
sakhawat6927 <rakiber357@gmail.com>
2025-08-08 20:19:18 +0800

    update home controller

--- mobile/RongTuli/lib/controllers/home_controller.dart
+++ mobile/RongTuli/lib/controllers/home_controller.dart
@@ -1,5 +1,15 @@
 import 'package:get/get.dart';
+import 'package:rong_tuli/consts/firebase_consts.dart';
 
 class HomeController extends GetxController{
   var currentNavIndex = 0.obs;
+  
+  var username = '';
+  getUsername() async {
+    await firestore.collection(userCollection).where('id', isEqualTo: currentUser!.uid).get().then((value){
+      if (value.docs.isNotEmpty){
+        return value.docs.single['name'];
+      }
+    });
+  }
 }
