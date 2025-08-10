cc8048751bc747afdc664cbdd0b9e274a75d9f38
sakhawat6927 <rakiber357@gmail.com>
2025-08-10 18:42:00 +0800

    update chat controller

--- mobile/RongTuli/lib/controllers/chat_controller.dart
+++ mobile/RongTuli/lib/controllers/chat_controller.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 import 'package:rong_tuli/consts/firebase_consts.dart';
@@ -17,4 +18,45 @@ var currentId = currentUser!.uid;
 var msgController = TextEditingController();
 
 dynamic chatDocId;
+
+getChatId() async {
+  await chats.where('users', isEqualTo: {
+    friendId : null,
+    currentId : null
+  }
+  ).limit(1).get().then((QuerySnapshot snapshot){
+    if(snapshot.docs.isNotEmpty){
+      chatDocId = snapshot.docs.single.id;
+    }else {
+      chats.add({
+        'created_on': null,
+        'last_msg' : '',
+        'users' : {friendId : null, currentId : null},
+        'toId' : '',
+        'fromId' : '',
+        'friend_name' : friendName,
+        'sender_name' : senderName
+      }).then((value){
+        chatDocId = value.id;
+      });
+    }
+  });
+}
+
+sendMsg (String msg) async {
+  if (msg.trim().isNotEmpty){
+    chats.doc(chatDocId).update({
+      'created_on' : FieldValue.serverTimestamp(),
+      'last_msg' : msg,
+      'toId' : friendId,
+      'fromId' : currentId,
+    });
+
+    chats.doc(chatDocId).collection(messageCollection).doc().set({
+      'created_on' : FieldValue.serverTimestamp(),
+      'msg' : msg,
+      'uid' : currentId,
+    });
+  }
+}
 }
